e8d8dcc6d505bf1fa0e3a95f64a21078491d9854
Steve Moreau <stevemoreau@users.noreply.github.com>
2020-03-23 09:13:11 +0100

    [A] Index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,38 +1,17 @@
 <template>
   <div class="container">
     <div>
-      <logo />
       <h1 class="title">
-        online-dev-tools
+        Some online tools
       </h1>
-      <h2 class="subtitle">
-        Online tools for Web and mobile developers
-      </h2>
       <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
+        <nuxt-link :to="{ name: 'color-opacity' }">
           Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+        </nuxt-link>
       </div>
     </div>
   </div>
 </template>
-
-<script>
-import Logo from '~/components/Logo.vue'
-
-export default {
-  components: {
-    Logo
-  }
-}
-</script>
 
 <style>
 .container {
